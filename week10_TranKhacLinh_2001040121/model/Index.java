eef2df837e77772da246a843f71d9c88745bd131
StebenC0912 <stebenc09122002@gmail.com>
2022-11-11 22:52:33 +0700

    update week10_TranKhacLinh_2001040121

---
+++ week10_TranKhacLinh_2001040121/model/Index.java
@@ -0,0 +1,14 @@
+package week10_TranKhacLinh_2001040121.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class Index {
+    private String document;
+    private Map<String, List<Integer>> entries;
+    
+    public Index(String document, Map<String, List<Integer>> entries){
+        this.document = document;
+        this.entries = entries;
+    }
+}
